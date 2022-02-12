b85d62f2357e400032e6add281363259af141df4
ioanniskalvis <ioanniskalvis@hotmail.com>
2022-02-12 17:09:30 +0000

    small change

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -1,8 +1,58 @@
-import React, { useContext } from 'react';
-import { AuthContext } from './AuthProvider';
+import type { NextPage } from 'next';
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+import apiInstance from '../axios';
+import styles from '../styles/Home.module.css';
+import { useAuth } from './AuthProvider';
 
-const Login = () => {
-  return <a>Login</a>;
-};
+export default function Profile() {
+  const router = useRouter();
+  const [hasMounted, setHasMounted] = useState<boolean>(false);
+  const { setAccessToken, login, token } = useAuth();
 
-export default Login;
+  const fetchUserData = async (bearerToken: string) => {
+    try {
+      const { data } = await apiInstance.get('/user/profile', {
+        headers: { Authorization: 'Bearer ' + bearerToken },
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const logout = async () => {
+    try {
+      const response = await apiInstance.get('/user/logout', {
+        headers: { Authorization: 'Bearer ' + token },
+      });
+
+      if (response.status === 200) {
+        router.push('/login');
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    if (router.isReady) {
+      const { access_token, error } = router.query;
+      if (error) {
+        router.push('/login');
+      } else if (access_token && typeof access_token === 'string') {
+        login();
+        setAccessToken(access_token);
+        fetchUserData(access_token);
+      } else {
+        fetchUserData(token);
+      }
+    }
+  }, [router.isReady]);
+
+  return (
+    <main>
+      <button onClick={logout}>Logout</button>
+      <a href="http://localhost:7777/user/login">Login</a>
+    </main>
+  );
+}
